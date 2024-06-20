25cbc7b40418dcbc280c9e32279ed42e2f06e14d
josepalloM <jose.pallo.md@gmail.com>
2024-06-20 10:46:48 -0500

    First commit script UFT

---
+++ TestWorkShop1/TestWeb1WK/TestWeb1WK/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\SAGGA-SERVICIOS\\Desktop\\CODE\\UFTWorkshop\\TestWorkShop1\\TestWeb1WK\\TestWeb1WK\\Res1',namePairs:[]}]};
